e2e517b2a29a399fd9a781c898f799efbe964f00
Theo GABELLON <thgabell@e3r7p7.clusters.42paris.fr>
2023-03-24 19:21:00 +0100

    no leaks mlx

--- srcs/init.c
+++ srcs/init.c
@@ -6,7 +6,7 @@
 /*   By: thgabell <thgabell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 18:14:48 by thgabell          #+#    #+#             */
-/*   Updated: 2023/03/23 18:25:09 by thgabell         ###   ########.fr       */
+/*   Updated: 2023/03/24 19:02:20 by thgabell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,12 +102,12 @@ int	read_map(t_fdf *s_fdf, char *argv)
 
 void	init_fdf(t_fdf *s_fdf)
 {
-	// s_fdf->mlx_ptr = mlx_init();
-	// s_fdf->win_ptr = mlx_new_window(s_fdf->mlx_ptr, 1920, 1080, "FDF");
-	// s_fdf->img_width = 1920;
-	// s_fdf->img_height = 1080;
-	// s_fdf->img_ptr = mlx_new_image(s_fdf->mlx_ptr, s_fdf->img_width, s_fdf->img_height);
-	// s_fdf->addr = mlx_get_data_addr(s_fdf->img_ptr, &s_fdf->bpp, &s_fdf->line_length, &s_fdf->endian);
+	s_fdf->mlx_ptr = mlx_init();
+	s_fdf->win_ptr = mlx_new_window(s_fdf->mlx_ptr, 1920, 1080, "FDF");
+	s_fdf->img_width = 1920;
+	s_fdf->img_height = 1080;
+	s_fdf->img_ptr = mlx_new_image(s_fdf->mlx_ptr, s_fdf->img_width, s_fdf->img_height);
+	s_fdf->addr = mlx_get_data_addr(s_fdf->img_ptr, &s_fdf->bpp, &s_fdf->line_length, &s_fdf->endian);
 	s_fdf->s_map.width = 0;
 	s_fdf->s_map.height = 0;
 	s_fdf->s_map.points = NULL;
